905b71acfd61633e0eb3b26ee7b3882be4e3dab9
dongyankun <dongyankun12@163.com>
2020-12-14 17:48:04 +0800

    修改问题

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,6 +19,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
     initMeiQia();
   }
+
   Future<void> initMeiQia() async {
     try {
       res = await FlutterMeiqia.initMeiQia("4e9af9c6e9fb0cf1274ae34b2a952ddd");
@@ -32,40 +33,36 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      supportedLocales: [const Locale('zh', 'CH')],
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('客服Example'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Center(
-              child: Text('Running on: $res\n'),
-            ),
-            TextField(
-              onChanged: (s){
-                id = s;
-              },
-              controller: TextEditingController(text: id),
-            ),
-            FlatButton(
-                onPressed: () async {
-                  //562849003
-                  Map<String,String> clientInfo = new Map();
-                  clientInfo["userId"] = "";
-                  clientInfo["订单ID"] = "";
-                  clientInfo["商品ID"] = "";
+          body: Column(
+        children: <Widget>[
+          // Center(
+          //   child: Text('Running on: $res\n'),
+          // ),
+          // TextField(
+          //   onChanged: (s) {
+          //     id = s;
+          //   },
+          //   controller: TextEditingController(text: id),
+          // ),
+          FlatButton(
+              onPressed: () async {
+                //562849003
+                Map<String, String> clientInfo = new Map();
+                clientInfo["userId"] = "";
+                clientInfo["订单ID"] = "";
+                clientInfo["商品ID"] = "";
 
-                   await FlutterMeiqia.openMeiQia(id: "576929681",userInfo: clientInfo,isUpdate: true);
-                },
-                child: Text("打开客服")
-            )
-          ],
-        )
-      ),
+                await FlutterMeiqia.openMeiQia(
+                    id: "576929681", userInfo: clientInfo, isUpdate: true);
+              },
+              child: Text("打开客服"))
+        ],
+      )),
     );
   }
 }
--- lib/flutter_meiqia.dart
+++ lib/flutter_meiqia.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterMeiqia {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_meiqia');
+  static const MethodChannel _channel = const MethodChannel('flutter_meiqia');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
@@ -12,12 +11,15 @@ class FlutterMeiqia {
   }
 
   static Future<String> initMeiQia(String appKey) async {
-    final String res = await _channel.invokeMethod('initMeiQia',{"appKey":appKey});
+    final String res =
+        await _channel.invokeMethod('initMeiQia', {"appKey": appKey});
     return res;
   }
 
-  static Future<String> openMeiQia({String id,Map<String,String> userInfo,bool isUpdate}) async {
-    final String res = await _channel.invokeMethod('openMeiQia',{"id":id,"userInfo":userInfo,"isUpdate":isUpdate});
+  static Future<String> openMeiQia(
+      {String id, Map<String, String> userInfo, bool isUpdate}) async {
+    final String res = await _channel.invokeMethod(
+        'openMeiQia', {"id": id, "userInfo": userInfo, "isUpdate": isUpdate});
     return res;
   }
 }
